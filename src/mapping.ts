70441c9be9cbc31fb6adf60913cd97df0e41a427
Anton Yip <anton_yip@hotmail.com>
2022-05-03 12:32:31 +0800

    code to run avax

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,9 +1,16 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts"
 import {
   ERC20,
 } from "../generated/ERC20/ERC20"
 import { BlockEntity } from "../generated/schema"
 
-export function handleBlock(): void {
-  // BlockEntity b = new BlockEntity()
+var prevBlockTimestamp = new BigInt(0);
+
+export function handleBlock(block: ethereum.Block): void {
+  var b = new BlockEntity(block.number.toString());
+  b.timestamp = block.timestamp;
+  b.prevtimestamp = prevBlockTimestamp;
+  prevBlockTimestamp = b.timestamp;
+  b.diff = b.timestamp.minus(b.prevtimestamp);
+  b.save();
 }
