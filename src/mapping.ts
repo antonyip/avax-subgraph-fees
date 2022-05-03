719f99d8999c4f5a071e5d6ec4b9d3000b34db75
Anton Yip <anton_yip@hotmail.com>
2022-05-03 12:49:17 +0800

    stuffs

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,13 +4,31 @@ import {
 } from "../generated/ERC20/ERC20"
 import { BlockEntity } from "../generated/schema"
 
-var prevBlockTimestamp = new BigInt(0);
+var BIGZERO = new BigInt(0);
+var BIGONE = new BigInt(1)
+
+export function getOrCreateBlock(id: string): BlockEntity {
+  var result = BlockEntity.load(id);
+  if (result === null)
+  {
+    result = BlockEntity.load('0');
+  }
+
+  if (result === null)
+  {
+    result = new BlockEntity('0');
+    result.timestamp = new BigInt(0);
+    result.save();
+  }
+
+  return result;
+}
 
 export function handleBlock(block: ethereum.Block): void {
-  var b = new BlockEntity(block.number.toString());
+  var b = getOrCreateBlock(block.number.toString());
+  var bPrev = getOrCreateBlock(block.number.minus(BIGONE).toString());
   b.timestamp = block.timestamp;
-  b.prevtimestamp = prevBlockTimestamp;
-  prevBlockTimestamp = b.timestamp;
-  b.diff = b.timestamp.minus(b.prevtimestamp);
+  b.prevtimestamp = bPrev.timestamp;
+  b.diff = b.timestamp.minus(bPrev.timestamp);
   b.save();
 }
