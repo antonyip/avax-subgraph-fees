277d10aa15c94124e67fcaf761268478b6c00204
Anton Yip <anton_yip@hotmail.com>
2022-05-03 13:17:28 +0800

    checkpoint, failure, just remove math and it should work

--- src/mapping.ts
+++ src/mapping.ts
@@ -9,26 +9,34 @@ var BIGONE = new BigInt(1)
 
 export function getOrCreateBlock(id: string): BlockEntity {
   var result = BlockEntity.load(id);
-  if (result === null)
-  {
-    result = BlockEntity.load('0');
-  }
 
   if (result === null)
   {
-    result = new BlockEntity('0');
-    result.timestamp = new BigInt(0);
-    result.save();
+    result = new BlockEntity(id);
   }
 
   return result;
 }
 
 export function handleBlock(block: ethereum.Block): void {
+
+  // init historical values
+  var block0 = getOrCreateBlock('0');
+  block0.blocknum = BIGZERO;
+  block0.timestamp = block0.timestamp ? block0.timestamp : BIGZERO;
+  
+
+  // create and set new block
   var b = getOrCreateBlock(block.number.toString());
-  var bPrev = getOrCreateBlock(block.number.minus(BIGONE).toString());
   b.timestamp = block.timestamp;
-  b.prevtimestamp = bPrev.timestamp;
-  b.diff = b.timestamp.minus(bPrev.timestamp);
+  b.blocknum = block.number;
+
+  // do math for previous blocks
+  b.prevtimestamp = block0.timestamp;
+  b.diff = b.timestamp.minus(block0.timestamp);
+  block0.timestamp = block.timestamp;
+
+  // save the results.
   b.save();
+  block0.save();
 }
