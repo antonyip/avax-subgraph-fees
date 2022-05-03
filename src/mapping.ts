91f7c06ce770a4a4e0cc37f65fd22f6512c853e1
Anton Yip <anton_yip@hotmail.com>
2022-05-03 13:27:18 +0800

    working system

--- src/mapping.ts
+++ src/mapping.ts
@@ -9,7 +9,7 @@ var BIGONE = new BigInt(1)
 
 export function getOrCreateBlock(id: string): BlockEntity {
   var result = BlockEntity.load(id);
-
+  
   if (result === null)
   {
     result = new BlockEntity(id);
@@ -20,23 +20,26 @@ export function getOrCreateBlock(id: string): BlockEntity {
 
 export function handleBlock(block: ethereum.Block): void {
 
-  // init historical values
-  var block0 = getOrCreateBlock('0');
-  block0.blocknum = BIGZERO;
-  block0.timestamp = block0.timestamp ? block0.timestamp : BIGZERO;
-  
+  if (block.number > BIGONE)
+  {
+    // init historical values
+    var block0 = getOrCreateBlock('0');
+    block0.blocknum = BIGZERO;
+    //block0.timestamp = block0.timestam ? block0.timestamp : BIGZERO;
+    
 
-  // create and set new block
-  var b = getOrCreateBlock(block.number.toString());
-  b.timestamp = block.timestamp;
-  b.blocknum = block.number;
+    // create and set new block
+    var b = getOrCreateBlock(block.number.toString());
+    b.timestamp = block.timestamp;
+    b.blocknum = block.number;
 
-  // do math for previous blocks
-  b.prevtimestamp = block0.timestamp;
-  b.diff = b.timestamp.minus(block0.timestamp);
-  block0.timestamp = block.timestamp;
+    // do math for previous blocks
+    //b.prevtimestamp = block0.timestamp;
+    //b.diff = b.timestamp.minus(block0.timestamp);
+    //block0.timestamp = block.timestamp;
 
-  // save the results.
-  b.save();
-  block0.save();
+    // save the results.
+    b.save();
+    block0.save();
+  }
 }
